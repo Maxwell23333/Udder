c50071788069fec0eec69f8ca916e385da087e57
Maxwell23333 <60060270+Maxwell23333@users.noreply.github.com>
2023-09-16 17:10:35 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Maxwell Elon on 2023/9/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
